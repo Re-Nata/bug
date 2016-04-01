a74c20e2c59c0d563317fc4fbe06ba81b2f70821
Nata <nataliya.pyrharu@gmail.com>
2016-04-01 16:27:48 +0300

    Changes with modals (again....) and also on home page

--- main.js
+++ main.js
@@ -167,25 +167,5 @@ $('#overlay').click( function(){
     });
 });
 
-//for I will go (button) when person is not logged-in: schedule-tab
-$(document).ready(function() {
-        $('a#go-button').click( function(event){
-            event.preventDefault();
-        $('#background').fadeIn(
-            function(){
-                $('#notification3')
-                .css('display', 'block')
-                .animate({opacity: 1, top: '40%'}, 200);
-        });
-});
 
-$('#background').click( function(){
-    $('#notification3')
-    .animate({opacity: 0, top: '40%'}, 200,
-    function(){
-        $(this).css('display', 'none');
-        $('#background').fadeOut(400);
-    }
-        );
-    });
-});
+
